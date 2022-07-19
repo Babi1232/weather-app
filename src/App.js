8b8b7e23a5d28f81554e8a27fda8031422699654
babisha <Babisha.shrestha1995@gmail.com>
2022-07-19 21:46:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div>
       <h1> hello</h1>
       <footer>
-        This project is coded by Babisha Shrestha.
+        This project is coded by Babisha Shrestha{""}
         <a href="https://github.com/Babi1232/weather-app" target="_blank">
           open-source on Github
         </a>
