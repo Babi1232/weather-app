f6da5a794a2697885089ac8abaef69570a6f89e1
babisha <Babisha.shrestha1995@gmail.com>
2022-07-31 17:30:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project is coded by Babisha Shrestha{" "}
+          This project is coded by Babisha Shrestha on{" "}
           <a href="https://github.com/Babi1232/weather-app">
             {" "}
             Open-source on github
-          </a>
+          </a>{" "}
+          and{" "}
+          <a href="https://deft-scone-7ed7cb.netlify.app/">hosted on netlify</a>
         </footer>
       </div>
     </div>
